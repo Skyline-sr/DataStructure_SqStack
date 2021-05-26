fa23141c8697cd01ddb5aded5742b4894eab380a
睿 宋 <1656965463@qq.com>
2021-05-26 23:42:57 +0800

    编写栈打印函数，测试初始化、Push、打印功能

--- DataStructure_SqStack.cpp
+++ DataStructure_SqStack.cpp
@@ -10,6 +10,8 @@ typedef struct {
 	int stacksize;
 }SqStack;
 
+void main();
+
 Status InitStack(SqStack& S);
 Status StackEmpty(SqStack S);
 int StackLength(SqStack S);
@@ -18,9 +20,23 @@ Status DestroyStack(SqStack& S);
 Status Push(SqStack& S, SElemType x);
 Status Pop(SqStack& S, SElemType* x);
 Status GetTop(SqStack* S, SElemType* x);
+void PrintStack(SqStack S);
 
 void main() {
 	SqStack S;
+	int i, i_push;
+	SElemType x;
+	InitStack(S);
+	printf("请输入要压入多少个的数据：");
+	scanf("%d", &i_push);
+	for ( i = 1; i <= i_push; i++)
+	{
+		printf("第%d个元素：", i);
+		scanf("%d",&x);
+		Push(S, x);
+	}
+	printf("长度为：%d\n",StackLength(S));
+	PrintStack(S);
 }
 
 Status InitStack(SqStack& S)
@@ -100,4 +116,14 @@ Status GetTop(SqStack* S, SElemType* x) {
 	}
 	*x = *(S->top - 1);
 	return OK;
+}
+
+void PrintStack(SqStack S) {
+	int length,i;
+	length = StackLength(S);
+	for ( i = length; i >0; i--)
+	{
+		S.top--;
+		printf("第%d个位置的元素：%d\n", i, *S.top);
+	}
 }
