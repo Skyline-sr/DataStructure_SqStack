760c5bd230459533284876da37e044217e8a2e1f
睿 宋 <1656965463@qq.com>
2021-05-26 23:22:32 +0800

    主要功能函数完成

--- DataStructure_SqStack.cpp
+++ DataStructure_SqStack.cpp
@@ -1,7 +1,103 @@
 ﻿#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
 
+#define MAXSIZE 100
 
+typedef struct {
+	SElemType* top;
+	SElemType* base;
+	int stacksize;
+}SqStack;
+
+Status InitStack(SqStack& S);
+Status StackEmpty(SqStack S);
+int StackLength(SqStack S);
+Status ClearStack(SqStack& S);
+Status DestroyStack(SqStack& S);
+Status Push(SqStack& S, SElemType x);
+Status Pop(SqStack& S, SElemType* x);
+Status GetTop(SqStack* S, SElemType* x);
 
 void main() {
-	
+	SqStack S;
+}
+
+Status InitStack(SqStack& S)
+{
+	S.base = new SElemType[MAXSIZE];
+	if (!S.base)
+	{
+		exit(OVERFLOW);
+	}
+	S.top = S.base;
+	S.stacksize = MAXSIZE;
+	return OK;
+}
+
+Status StackEmpty(SqStack S) {
+	if (S.top == S.base)
+	{
+		return TRUE;
+	}
+	else
+	{
+		return FALSE;
+	}
+}
+
+int StackLength(SqStack S) {
+	return (S.top - S.base);
+}
+
+Status ClearStack(SqStack& S) {
+	if (S.base)
+	{
+		S.top == S.base;
+		return OK;
+	}
+	return ERROR;
+}
+
+Status DestroyStack(SqStack& S) {
+	if (S.base)
+	{
+		delete S.base;
+		S.base = S.top = NULL;
+		S.stacksize = 0;
+		return OK;
+	}
+	return ERROR;
+}
+
+Status Push(SqStack& S, SElemType x) {
+	if (StackLength(S) == S.stacksize)
+	{
+		printf("栈已满！");
+		return ERROR;
+	}
+	*S.top = x;
+	S.top++;
+	return OK;
+}
+
+Status Pop(SqStack& S, SElemType* x) {
+	if (S.top == S.base)
+	{
+		printf("栈为空，无元素出栈！");
+		return ERROR;
+	}
+	S.top--;
+	*x = *S.top;
+	return OK;
+}
+
+Status GetTop(SqStack* S, SElemType* x) {
+	if (S->top == S->base)
+	{
+		printf("栈为空！");
+		return ERROR;
+	}
+	*x = *(S->top - 1);
+	return OK;
 }
---
+++ main.h
@@ -0,0 +1,15 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define TRUE 1
+#define FALSE 0
+#define OK 1
+#define ERROR 0
+#define INFEASIBLE -1
+#define OVERFLOW -2
+
+typedef int Status;
+typedef int SElemType;
+
+#endif
+
